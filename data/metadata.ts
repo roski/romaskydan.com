ec52c462756f65e82bba3c3c982d5fd575cbf506
Roman Skydan <roma.skidan@gmail.com>
2024-04-25 22:12:04 +0300

    Added base site metadata and changed prettier printWidth config

---
+++ data/metadata.ts
@@ -0,0 +1,33 @@
+import { Metadata } from 'next';
+import type { OpenGraph } from 'next/dist/lib/metadata/types/opengraph-types';
+
+/** Blog information */
+export const siteInfo = {
+  url: 'https://romaskydan.com',
+  title: 'Roman Skydan',
+  description: 'Developer, writer, and creator.',
+  socialCardUrl: '/images/social-card.png',
+  language: 'en-us',
+};
+
+/** Open Graph metadata */
+const openGraphMetadata: OpenGraph = {
+  title: siteInfo.title,
+  description: siteInfo.description,
+  url: siteInfo.url,
+  siteName: siteInfo.title,
+  images: [siteInfo.socialCardUrl],
+  locale: 'en_US',
+  type: 'website',
+};
+
+/** Base site metadata */
+export const baseMetadata: Metadata = {
+  metadataBase: new URL(siteInfo.url),
+  title: {
+    default: siteInfo.title,
+    template: `%s | ${siteInfo.title}`,
+  },
+  description: siteInfo.description,
+  openGraph: openGraphMetadata,
+};
