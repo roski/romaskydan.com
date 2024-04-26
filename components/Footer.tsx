27b8ba0b16c46817f02c1fbff4f49f082da94024
Roman Skydan <roma.skidan@gmail.com>
2024-04-26 23:41:20 +0300

    Added structure of Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,14 @@
+import { siteInfo } from '@/data/metadata';
+import Link from 'next/link';
+
 const Footer = () => {
-  return <footer>footer</footer>;
+  return (
+    <footer className="flex justify-center gap-x-2 p-5">
+      <Link href="/">{siteInfo.author}</Link>
+      <div>{` • `}</div>
+      <div>{`© ${new Date().getFullYear()}`}</div>
+    </footer>
+  );
 };
 
 export default Footer;
