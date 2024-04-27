6c8c7a61fdd58d48f62e715665786ec5268d0c49
Roman Skydan <roma.skidan@gmail.com>
2024-04-27 21:43:03 +0300

    Added empty page for blog and work also improved footer styles

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center gap-x-2 p-5">
+    <footer className="section flex justify-center gap-x-2 p-5">
       <Link href="/">{siteInfo.author}</Link>
       <div>{` • `}</div>
       <div>{`© ${new Date().getFullYear()}`}</div>
