b19f7cc08f478b6a2f04669531e5620d04b39829
Roman Skydan <roma.skidan@gmail.com>
2024-04-27 20:44:36 +0300

    Renamed Header and Footer files

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import './globals.css';
 import { baseMetadata, siteInfo } from '@/data/metadata';
 import { ThemeProvider } from 'next-themes';
 import { Analytics } from '@vercel/analytics/react';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import Header from '@/components/header';
+import Footer from '@/components/footer';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
