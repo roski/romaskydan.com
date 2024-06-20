21de6d4a583d49dbab7ea6a25e20b73f4395a511
Roman Skydan <roma.skidan@gmail.com>
2024-06-21 01:07:07 +0300

    Fixed tittle sizes for different pages

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,7 +6,7 @@ export default function BlogPage() {
   const blogPosts = sortPublicationByDate(posts);
   return (
     <section className="flex w-full flex-col gap-5">
-      <h1 className="text-4xl font-semibold">Blog</h1>
+      <h1 className="text-4xl font-bold">Blog</h1>
       <PostList posts={blogPosts} />
     </section>
   );
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,7 +6,7 @@ export default function ProjectsPage() {
   const sortedProjects = sortPublicationByDate(projects);
   return (
     <section className="flex w-full flex-col gap-5">
-      <h1 className="text-4xl font-semibold">Projects</h1>
+      <h1 className="text-4xl font-bold">Projects</h1>
       <ProjectList projects={sortedProjects} />
     </section>
   );
